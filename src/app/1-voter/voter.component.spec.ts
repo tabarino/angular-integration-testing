31c430728a9526e745c348455bdc94ed2125b4de
tabarino <tabarino@outlook.com>
2020-06-27 10:44:30 +0100

    264. Testing Property Bindings

--- src/app/1-voter/voter.component.spec.ts
+++ src/app/1-voter/voter.component.spec.ts
@@ -1,19 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { VoterComponent } from './voter.component';
+import { By } from '@angular/platform-browser';
 
 describe('VoterComponent', () => {
     let component: VoterComponent;
     let fixture: ComponentFixture<VoterComponent>;
 
-    beforeEach(() => {
+    beforeEach(async(() => {
         TestBed.configureTestingModule({
             declarations: [VoterComponent]
-        });
+        }).compileComponents();
+    }));
 
+    beforeEach(() => {
         fixture = TestBed.createComponent(VoterComponent);
         component = fixture.componentInstance;
+        fixture.detectChanges();
     });
 
-    it('should create', () => {
+    it('should render the total votes', () => {
+        component.othersVote = 20;
+        component.myVote = 1;
+        fixture.detectChanges();
+
+        const de = fixture.debugElement.query(By.css('.vote-count'));
+        const el: HTMLElement = de.nativeElement;
+
+        expect(el.innerText).toContain('21');
+    });
+
+    it('should highlight the upvote button if I have upvoted', () => {
+        component.myVote = 1;
+        fixture.detectChanges();
+
+        const de = fixture.debugElement.query(By.css('.glyphicon-menu-up'));
+
+        expect(de.classes.highlighted).toBeTruthy();
     });
 });
